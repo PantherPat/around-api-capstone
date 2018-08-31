20c2abe0b37c7f090f2768c712870225082dd003
Patrick Keshishian <Patrick.keshishian@gmail.com>
2018-08-31 17:54:14 -0400

    look and feel finalized

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+//define default gps coordinates for New York
+let lat = "40.730610";
+let long = "-73.935242";
+
+function getTicketmaster() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(saveLocation);
+    } else {
+        console.log('Geolocation is not supported by this browser.');
+    }
+}
+
+function saveLocation(position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    getTicketmasterApi(lat, long);
+}
+
+function getTicketmasterApi(lat, long, keywordInput) {
+    /* Update all the parameters for your API test*/
+    var params = {
+        apikey: 'gTqUrGAhPVOo80W5dwim0PACPObENQ0h',
+        keyword: keywordInput,
+        //            location: lat + ',' + long,
+        within: 25,
+    };
+    var result = $.ajax({
+            /* update API end point */
+            url: 'https://app.ticketmaster.com/discovery/v2/events?&countryCode=US',
+            data: params,
+            dataType: "json",
+            /*set the call type GET / POST*/
+            type: "GET"
+        })
+
+        /* if the call is successful (status 200 OK) show results */
+        .done(function (result) {
+            /* if the results are meeningful, we can just console.log them */
+            console.log(result);
+            displayTicketmaster(result);
+        })
+        /* if the call is NOT successful show errors */
+        .fail(function (jqXHR, error, errorThrown) {
+            console.log(jqXHR);
+            console.log(error);
+            console.log(errorThrown);
+        });
+}
+
+function displayTicketmaster(data) {
+    console.log('In displayEventful');
+    console.log(data); // NOTE - PROGRAM THROWS ERROR BEFORE THIS POINT, SO HAVEN'T VIEWED OUTPUT
+    const results = data._embedded.events.map((item, index) => renderTicketmaster(item)); // THIS NEEDS TO BE EDITED ONCE I CAN SEE THE OBJECT
+}
+
+function renderTicketmaster(result) {
+    console.log(result);
+    let buildTheHtmlOutput = "<li>";
+    buildTheHtmlOutput += "<h2>" + result.name + "</h2>";
+    buildTheHtmlOutput += "<a href='" + result.url + "' target='_blank'>";
+    buildTheHtmlOutput += "<p> Start date: " + result.dates.start.localDate + "</p>";
+    buildTheHtmlOutput += "<p> Location: " + result._embedded.venues[0].name + "</p>";
+    if ((result.images[0].url) && (result.images[0].url != "") && (result.images[0].url != undefined)) {
+        buildTheHtmlOutput += "<img id='eventImages' src='" + result.images[0].url + "'/>";
+    }
+    buildTheHtmlOutput += "</a>";
+    buildTheHtmlOutput += "</li>";
+    //use the HTML output to show it in the index.html
+    $("#result-list ul").append(buildTheHtmlOutput);
+}
+
+
 $(document).ready(function () {
     //do stuff
     $('main').hide();
@@ -11,9 +83,6 @@ $(document).on('click', '.button', function (event) {
     $('main').hide();
     $('.search-results').show();
     $('#result-list').hide();
-    $(function () {
-        $("#date-value").datepicker();
-    });
 
 });
 
@@ -26,82 +95,8 @@ $(document).submit('#search-form', function (event) {
     $('#result-list').show();
     const locationInput = $("#location-value").val();
     const keywordInput = $("#keyword-value").val();
-    const dateInput = $("#date-value").val();
 
-    console.log(locationInput, keywordInput, dateInput);
-    $(document).removeClass("background-image");
-
-    //define default gps coordinates for New York
-    let lat = "40.730610";
-    let long = "-73.935242";
-
-    function getTicketmaster() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(saveLocation);
-        } else {
-            console.log('Geolocation is not supported by this browser.');
-        }
-    }
-
-    function saveLocation(position) {
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
-        getTicketmasterApi(lat, long);
-    }
-
-    function getTicketmasterApi(lat, long) {
-        /* Update all the parameters for your API test*/
-        var params = {
-            apikey: 'gTqUrGAhPVOo80W5dwim0PACPObENQ0h',
-            keyword: keywordInput,
-            //            location: lat + ',' + long,
-            dateTime: dateInput,
-            within: 25,
-        };
-        var result = $.ajax({
-                /* update API end point */
-                url: 'https://app.ticketmaster.com/discovery/v2/events?&countryCode=US',
-                data: params,
-                dataType: "json",
-                /*set the call type GET / POST*/
-                type: "GET"
-            })
-
-            /* if the call is successful (status 200 OK) show results */
-            .done(function (result) {
-                /* if the results are meeningful, we can just console.log them */
-                console.log(result);
-                displayTicketmaster(result);
-            })
-            /* if the call is NOT successful show errors */
-            .fail(function (jqXHR, error, errorThrown) {
-                console.log(jqXHR);
-                console.log(error);
-                console.log(errorThrown);
-            });
-    }
-
-    function displayTicketmaster(data) {
-        console.log('In displayEventful');
-        console.log(data); // NOTE - PROGRAM THROWS ERROR BEFORE THIS POINT, SO HAVEN'T VIEWED OUTPUT
-        const results = data._embedded.events.map((item, index) => renderTicketmaster(item)); // THIS NEEDS TO BE EDITED ONCE I CAN SEE THE OBJECT
-    }
-
-    function renderTicketmaster(result) {
-        console.log(result);
-        let buildTheHtmlOutput = "<li>";
-        buildTheHtmlOutput += "<h2>" + result.name + "</h2>";
-        buildTheHtmlOutput += "<a href='" + result.url + "' target='_blank'>";
-        buildTheHtmlOutput += "<p> Start date: " + result.dates.start.localDate + "</p>";
-        buildTheHtmlOutput += "<p> Location: " + result._embedded.venues[0].name + "</p>";
-        if ((result.images[0].url) && (result.images[0].url != "") && (result.images[0].url != undefined)) {
-            buildTheHtmlOutput += "<img id='eventImages' src='" + result.images[0].url + "'/>";
-        }
-        buildTheHtmlOutput += "</a>";
-        buildTheHtmlOutput += "</li>";
-        //use the HTML output to show it in the index.html
-        $("#result-list ul").append(buildTheHtmlOutput);
-    }
-    //    $(getEventful);
-    $(getTicketmaster);
+    console.log(locationInput, keywordInput, );
+    $(".half-column").removeClass("background-image");
+    getTicketmasterApi(lat, long, keywordInput);
 });
